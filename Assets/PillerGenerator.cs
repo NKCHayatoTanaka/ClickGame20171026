653d8e20f821325656f183d48b64205d14f998a0
Hayato Tanaka <nkc.tanaka.hayato@gmail.com>
2017-10-26 11:08:39 +0900

    柱設置

--- Assets/PillerGenerator.cs
+++ Assets/PillerGenerator.cs
@@ -3,11 +3,19 @@ using System.Collections;
 
 public class PillerGenerator : MonoBehaviour
 {
+    public GameObject pillerPrefab;
 
     // Use this for initialization
     void Start()
     {
-
+        for (int i = 0; i < 20; i++)
+        {
+            for (int j = 0; j < 20; j++)
+            {
+                GameObject Piller = Instantiate(pillerPrefab) as GameObject;
+                Piller.transform.position=new Vector3 (i, 0, j);
+            }
+        }
     }
 
     // Update is called once per frame
